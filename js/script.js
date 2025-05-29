bbf604f44853da97425ea03c3de9ca69a993f2a3
Anika <anikatahsinn0@gmail.com>
2025-05-29 16:55:49 +1000

    scroll animation added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const scrollWrapper = document.querySelector('.scroll-wrapper');
+
+function smoothScroll() {
+    if (scrollWrapper.scrollTop < (scrollWrapper.scrollHeight - scrollWrapper.clientHeight)) {
+    scrollWrapper.scrollBy({ top: 1, behavior: 'smooth' });
+    requestAnimationFrame(smoothScroll);
+    }
+}
+
+setTimeout(() => {
+    smoothScroll();
+}, 1000);
