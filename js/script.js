65e758dbd13099cbf5ea97666389a46a1eaec237
Anika <anikatahsinn0@gmail.com>
2025-05-29 16:59:23 +1000

    all files

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const scrollWrapper = document.querySelector('.scroll-wrapper');
 
 function smoothScroll() {
-    if (scrollWrapper.scrollTop < (scrollWrapper.scrollHeight - scrollWrapper.clientHeight)) {
+  if (scrollWrapper.scrollTop < (scrollWrapper.scrollHeight - scrollWrapper.clientHeight)) {
     scrollWrapper.scrollBy({ top: 1, behavior: 'smooth' });
     requestAnimationFrame(smoothScroll);
-    }
+  }
 }
 
 setTimeout(() => {
-    smoothScroll();
+  smoothScroll();
 }, 1000);
